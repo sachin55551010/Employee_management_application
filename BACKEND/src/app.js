196fd2cf6b2f57dafca2741fe707e1fea91f643a
Sachin Khatri <sachinbansal3333@gmail.com>
2025-09-16 18:09:34 +0530

    added backend link in cors

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -13,8 +13,12 @@ app.use(
   cors({
     origin: [process.env.FRONTEND_URL, process.env.PRODUCTION_FRONTEND_URL],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.options("*", cors()); // <-- handle preflight globally
 
 //routes
 app.get("/", (_, res) => {
